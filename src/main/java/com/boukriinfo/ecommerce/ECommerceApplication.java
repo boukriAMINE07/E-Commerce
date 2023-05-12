3ea5b4f55f14026fc845b1bbd72a4a804e7f3885
Amine boukri <amine.boukri-etu@etu.univh2c.ma>
2023-05-12 16:38:22 +0100

    add saveCategory

--- src/main/java/com/boukriinfo/ecommerce/ECommerceApplication.java
+++ src/main/java/com/boukriinfo/ecommerce/ECommerceApplication.java
@@ -1,10 +1,13 @@
 package com.boukriinfo.ecommerce;
 
 import com.boukriinfo.ecommerce.entities.Category;
+import com.boukriinfo.ecommerce.entities.ERole;
 import com.boukriinfo.ecommerce.entities.Product;
+import com.boukriinfo.ecommerce.entities.Role;
 import com.boukriinfo.ecommerce.repositories.CategoryRepository;
 import com.boukriinfo.ecommerce.repositories.ProductRepository;
 
+import com.boukriinfo.ecommerce.repositories.RoleRepository;
 import com.github.javafaker.Faker;
 
 import org.springframework.beans.BeanUtils;
@@ -25,6 +28,31 @@ public class ECommerceApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(ECommerceApplication.class, args);
+    }
+
+    @Bean
+    CommandLineRunner saveCategory(CategoryRepository categoryRepository){
+
+        return args -> {
+            Faker faker = new Faker();
+            for (int i = 0; i < 10; i++) {
+                Category category = new Category();
+                category.setName(faker.commerce().department());
+                category.setSlug(faker.commerce().department());
+                categoryRepository.save(category);
+            }
+        };
+    }
+    @Bean
+    CommandLineRunner saveRole(RoleRepository roleRepository){
+
+            return args -> {
+                Role role = new Role();
+                role.setName(ERole.ROLE_USER);
+                roleRepository.save(role);
+                role.setName(ERole.ROLE_ADMIN);
+                roleRepository.save(role);
+            };
     }
 
 
